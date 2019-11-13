8e6d305429491de89085ec0ccc441fc0a66c9352
James Lawlor <jlawlor@hudsonalpha.org>
2019-11-13 16:41:38 -0600

    Bsub option and add SVANN to filename

--- graph_vcf.py
+++ graph_vcf.py
@@ -7,43 +7,59 @@ import subprocess
 import re
 
 parser = argparse.ArgumentParser(description="Use IGV to graph BED regions")
-parser.add_argument('-r', '--bed', type=str, help="BED input filename", required=True)
-parser.add_argument('-t', '--title', type=int, help="BED title column", default=4)
+parser.add_argument('-v', '--vcf', type=str, help="VCF input filename", required=True)
 parser.add_argument('-b', '--bams', type=str, help="BAMS", nargs='+', required=True)
 parser.add_argument('-g', '--genome', type=str, default="hg38", help="Genome (hg19 or hg38)")
 parser.add_argument('-p', '--prefix', type=str, default="", help="File prefix.")
 parser.add_argument('-s', '--slop', type=int, default=0, help="Slop: Number of bases to add at beginning and end of graph")
 parser.add_argument('-n', '--indel-bp-threshold', default=1, help="Hide indels below this length.")
 parser.add_argument('--print', action="store_true", help="Print commands instead of executing them")
+parser.add_argument('--bsub', action="store_true", help="Submit via LSF to default queue with recommended resources.")
 
 
 args = parser.parse_args()
 
 
-if args.bed.endswith('gz'):
-    inputfile = gzip.open(args.bed, 'rt')
+if args.vcf.endswith('gz'):
+    inputfile = gzip.open(args.vcf, 'rt')
 else:
-    inputfile = open(args.bed, 'r')
+    inputfile = open(args.vcf, 'r')
 
 for line in inputfile:
     if line.startswith('#'):
         continue
     line = line.strip().split('\t')
-    if len(line) == 1: # handle bed files that might be space-delimited
-        line = line.strip().split(' ')
-        if len(line) == 1:
-            print('BED format error. {}'.format(line))
-            inputfile.close()
-            sys.exit()
-    chrom = line[0]
-    start = int(line[1])
-    end = int(line[2])
-    try:
-        name = re.sub(r"[:,. \*]", "_", line[args.title - 1]) # clean to prevent filename weirdness
-    except IndexError:
-        print("Invalid column selected with -t.")
+    if len(line) == 1:
+        print('VCF format error. {}'.format(line))
         inputfile.close()
         sys.exit()
+    chrom = line[0]
+    pos = int(line[1])
+    id = line[2]
+    ref = line[3]
+    alt = line[4]
+    qual = line[5]
+    filter = line[6]
+    info = line[7]
+    format = line[8]
+    info_dict = {x.split('=')[0]: x.split('=')[1] for x in info.split(';') if '=' in x}
+    #info_list = [x: True for x in info.split(';') if '=' not in x]
+    start = pos
+    if 'END' in info_dict:
+        end = int(info_dict['END'])
+    else:
+        end = start + 30
+
+    name = ""
+    if id != '.':
+        name = name + "_" + id
+    if 'SVTYPE' in info_dict:
+        name = name + "_" + info_dict['SVTYPE']
+    if 'SVANN' in info_dict:
+        name = name + "_" + info_dict['SVANN']
+
+    name = re.sub(r"[:,. \*]", "_", name) # clean to prevent filename weirdness
+
 
     filename = "_".join([chrom,str(start),str(end)])
     if name != "":
