908c9cc309f6507067793959de583feed8db0794
James Lawlor <jlawlor@hudsonalpha.org>
2021-09-02 18:02:55 -0500

    update IGV version and fix issues with not loading genes and with randomly crashing due to file locks and other BS

--- graph_region.sh
+++ graph_region.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 PATH=/gpfs/gpfs2/software/utils/xvfb:$PATH
 export IGV_MEM=16384m
-GENOME="hg38"
+GENOME="/cluster/lab/gcooper/igv-grapher/genomes/hg38/hg38.genome"
 INDEL_BP_THRESHOLD=1
 VIEW="expand"
 prefix=`dirname $(readlink $0 || echo $0)`
-
+#prefix=/cluster/lab/gcooper/software/IGV_Linux_2.11.0
 usage(){
   echo "$0 -b BAM_PATH [-b BAM_PATH] [-v expand|squish|collapse] -r REGION [-c CHROMOSOME] [-s START] [-e END] -o OUTPUT_FILENAME [-n MIN_INDEL_TO_SHOW] [-g GENOME]  ..." >&2
   exit
@@ -61,23 +61,7 @@ if [[ -n "$REGION" ]]; then
 fi
 
 echo \
-"preference SAM.MAX_VISIBLE_RANGE 1000000
-preference SAM.SHOW_CENTER_LINE true
-preference SAM.SMALL_INDEL_BP_THRESHOLD ${INDEL_BP_THRESHOLD}
-preference DEFAULT_VISIBILITY_WINDOW -1
-preference SAM.SHOW_SOFT_CLIPPED true
-preference SAM.FLAG_UNMAPPED_PAIR true
-preference SAM.FILTER_DUPLICATES false
-preference SAM.FILTER_SECONDARY_ALIGNMENTS false
-preference SAM.HIDDEN_TAGS XA,RG
-preference SAM.FILTER_FAILED_READS true
-preference SAM.FILTER_SUPPLEMENTARY_ALIGNMENTS false
-preference SAM.GROUP_OPTION BASE_AT_POS
-preference SHOW_REGION_BARS true
-preference SAM.QUALITY_THRESHOLD=-1
-preference SAM.GROUP_OPTION=SUPPLEMENTARY
-preference SAM.COLOR_BY=UNEXPECTED_PAIR
-preference SAM.GROUP_BY_TAG=HP
+"preference SAM.SMALL_INDEL_BP_THRESHOLD ${INDEL_BP_THRESHOLD}
 new
 genome ${GENOME}" >> ${COMMANDS_FILE}
 for B in ${BAMPATHS[@]}; do
@@ -91,5 +75,5 @@ sort start
 snapshot ${OUTPUT_FILENAME}
 exit" \
 >> ${COMMANDS_FILE}
-sed -i 's/-1=null/-1/' ~/igv/prefs.properties
+#sed -i 's/-1=null/-1/' ~/igv/prefs.properties
 xvfb-run-safe ${prefix}/igv.sh --batch ${COMMANDS_FILE}
--- graph_vcf.py
+++ graph_vcf.py
@@ -17,11 +17,12 @@ SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
 parser = argparse.ArgumentParser(description="Use IGV to graph BED regions")
 parser.add_argument('-v', '--vcf', type=str, help="VCF input filename", required=True)
 parser.add_argument('-b', '--bams', type=str, help="BAMS", nargs='+', required=True)
-parser.add_argument('-g', '--genome', type=str, default="hg38", help="Genome (hg19 or hg38)")
+parser.add_argument('-g', '--genome', type=str, default="/cluster/lab/gcooper/igv-grapher/genomes/hg38/hg38.genome", help="Genome (hg19 or hg38)")
 parser.add_argument('-p', '--prefix', type=str, default="", help="File prefix.")
 parser.add_argument('-s', '--slop', type=int, default=0, help="Slop: Number of bases to add at beginning and end of full graph")
 parser.add_argument('-k', '--breakpoint-slop', type=int, default=500, help="Number of bases to add at beginning and end of breakpoint graph")
 parser.add_argument('-n', '--indel-bp-threshold', default=10, help="Hide indels below this length.")
+parser.add_argument('-w', '--max-width', default=10, type=float, help="Largest width event to graph at once. (MB). Above this size will only graph breakpoints")
 parser.add_argument('--print', action="store_true", help="Print commands instead of executing them")
 parser.add_argument('--bsub', action="store_true", help="Submit via LSF to default queue with recommended resources.")
 #parser.add_argument('-r', '--regions', type=str, help="Regions of interest. Only print events intersecting these items.")
@@ -38,9 +39,12 @@ if args.vcf.endswith('gz'):
     inputfile = gzip.open(args.vcf, 'rt')
 else:
     inputfile = open(args.vcf, 'r')
-
+variant_text = tempfile.mkstemp(suffix='.txt', dir='/scratch/lab/gcooper/')[1]
+header = ""
 for line in inputfile:
     if line.startswith('#'):
+        if "CHROM" in line:
+            header = line.replace("\t", " ")
         continue
     line = line.strip().split('\t')
     if len(line) == 1:
@@ -55,10 +59,29 @@ for line in inputfile:
     qual = line[5]
     filter = line[6]
     info = line[7]
+    try:
+        rest = str(line[8:])
+    except IndexError:
+        rest = ""
     #format = line[8]
     info_dict = {x.split('=')[0]: x.split('=')[1] for x in info.split(';') if '=' in x}
     #info_list = [x: True for x in info.split(';') if '=' not in x]
     # Get original start and end. Do this first so we have a trace in filename
+
+    with open(variant_text, 'wt') as f:
+        f.write("HEADER: {}\n\n\n".format(header))
+        f.write("CHROM: {}\n\n\n".format(chrom))
+        f.write("POS: {}\n\n\n".format(pos))
+        f.write("ID: {}\n\n\n".format(id))
+        f.write("REF: {}\n\n\n".format(ref))
+        f.write("ALT: {}\n\n\n".format(alt))
+        f.write("QUAL: {}\n\n\n".format(qual))
+        f.write("FILTER: {}\n\n\n".format(filter))
+        for x in info_dict:
+            f.write("{}: {}\n\n\n".format(x, info_dict[x]))
+        f.write("GENOTYPES: {}\n\n\n".format(rest))
+
+
     svlen = int(info_dict.get('SVLEN', 0))
     start = pos
     try:
@@ -99,13 +122,6 @@ for line in inputfile:
     if args.prefix != "":
         filename = args.prefix + "_" + filename
 
-    # Modify start and end for special cases
-    #print(start,end)
-    if start == end: # grab insertions and change viewing area
-        start = start - svlen
-        end = end + svlen
-
-
     # Modify start and end with slop
     width = abs(end-start)
     if args.slop == 0:
@@ -113,9 +129,12 @@ for line in inputfile:
             expand = 0.10
         else:
             expand = 0.45
-        slop = max(int(float(width)*expand),100)
+        slop = max(int(float(width)*expand),500)
     else:
         slop = args.slop
+
+    #print("for main, calculated {} width, {} slop, {} start, {} end".format(width, slop, max(start - slop, 1), end + slop))
+
     command = ""
 
 
@@ -146,16 +165,18 @@ for line in inputfile:
 
     else:
         # Typical settings for the main graph
-        temps_and_settings.append(
-            {
-                'chrom': chrom,
-                'start': max(start - args.slop, 1),
-                'end': end + args.slop,
-                'name': tempfile.mkstemp(suffix='.png', dir='/scratch/lab/gcooper/')[1],
-                'indel': args.indel_bp_threshold
+        if width <= args.max_width*1000000:
+            temps_and_settings.append(
+                {
+                    'chrom': chrom,
+                    'start': max(start - slop, 1),
+                    'end': end + slop,
+                    'name': tempfile.mkstemp(suffix='.png', dir='/scratch/lab/gcooper/')[1],
+                    'indel': args.indel_bp_threshold
 
-            }
-        )
+                }
+            )
+
         if width > 1000: # If wider than a comfortable view, we will print each endpoint as well
             # left breakpoint
             temps_and_settings.append(
@@ -188,28 +209,33 @@ for line in inputfile:
         for bam in args.bams:
             command += ' -b {}'.format(bam)
         command += ' && '
-    command += SCRIPT_PATH + "/make_pdf.py -o " + filename + ".pdf " + " ".join(temps)
+
+    command += "/cluster/software/imagemagick-7.0.7/bin/mogrify -background white -alpha remove -alpha off " + " ".join(temps)
+    command += ' && '
+    command += SCRIPT_PATH + "/make_pdf.py --textfile " + variant_text + " -o " + filename + ".pdf " + " ".join(temps)
     #with open(filename + ".pdf", "wb") as pdf:
     #    pdf.write(img2pdf.convert(['all.png', 'left.png', 'right.png']))
 
     if args.print:
         print(command)
     else:
-        try:
-            if args.bsub:
-                command = 'bsub -R rusage[mem=24576] -n 1 -o igv_grapher.log ' + '"' + command + '"'
-            print("graphing {}".format(filename))
-            time.sleep(1)
-            result = subprocess.run(command, check=True, shell=True, capture_output=True, text=True)
-            if args.bsub:
-                print(result.stdout)
-                print(result.stderr)
-        except subprocess.CalledProcessError as e:
-            print("Captured error {}".format(e))
+
+        if args.bsub:
+            command = 'bsub -R rusage[mem=24576] -n 1 -o igv_grapher.log ' + '"' + command + '"'
+        print("graphing {}".format(filename))
+        #time.sleep(1)
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        if result.returncode != 0:
+            print("Captured error")
             print("****stdout****")
             print(result.stdout)
             print("****stderr****")
             print(result.stderr)
+            #sys.exit()
+        if args.bsub:
+            print(result.stdout)
+            print(result.stderr)
+
 
 # We ought to clean up the tempfiles that we've dumped into scratch, however if we're printing or bsubbing, this function exits before we're done with them
 
